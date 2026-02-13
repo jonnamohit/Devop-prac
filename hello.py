73b8d458c4c15b715b66e89ff168c1d4edeeca35
Sai Harshitha <VattemSai.Harshitha@mu-sigma.com>
2026-02-13 12:42:57 +0530

    modified codes

--- hello.py
+++ hello.py
@@ -4,3 +4,4 @@ b = 20
 c = a + b
 print("The sum of a and b is:", c)
 print ("This is a simple Python program to demonstrate basic syntax and operations.")
+print("We can perform various operations like addition, subtraction, multiplication, and division.")
