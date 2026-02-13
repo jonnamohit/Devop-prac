77719f4a936485a72d1ac7aa46260f7a9955b96b
Sai Harshitha <VattemSai.Harshitha@mu-sigma.com>
2026-02-13 13:01:08 +0530

    ssh and gpg keys

--- hello.py
+++ hello.py
@@ -5,3 +5,4 @@ c = a + b
 print("The sum of a and b is:", c)
 print ("This is a simple Python program to demonstrate basic syntax and operations.")
 print("We can perform various operations like addition, subtraction, multiplication, and division.")
+print("Let's perform some operations:")
